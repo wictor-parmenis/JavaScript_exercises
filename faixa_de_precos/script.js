7d54d79ef7dbbe2406c3d2f27afb9416b9a90adf
wictor-parmenis <wictortec@gmail.com>
2021-01-01 17:00:18 -0300

    Erase exercises not completes and i create a new exercise called: faixa_de_precos.

---
+++ faixa_de_precos/script.js
@@ -0,0 +1,43 @@
+/*
+O objetivo aqui é retornar produtos ao usuário final, de acordo com a afaixa 
+de preço escolhida pelo usuário.
+*/
+
+
+let results = document.getElementById('results')
+let products = 
+// Properties of object.
+{
+    name : ['1. Iphone X', '2. Iphone 12', '3. Galaxy S20', '4. Xbox control', '5. Sound Sony',
+     '6 Headphone JBL', '7. Galaxy A10'],
+    price : [700, 1200, 1100, 100, 120, 230, 300],
+    inStock : [true, true, true, true, true, true, false]
+};
+function bitPrice(){
+    results.innerHTML = `<p></p>`;
+    for (low_price in products.price){
+        if (products.price[low_price] <= 300){
+            results.innerHTML += `${products.name[low_price]} | ${products.price[low_price]} <br>`
+        }       
+    }       
+}
+
+function mediumPrice(){
+    results.innerHTML = `<p></p>`;
+    for (high_price in products.price){
+        if (products.price[high_price] > 300 & products.price[high_price] <= 700){
+            results.innerHTML += `${products.name[high_price]} | ${products.price[high_price]} <br>`
+        }       
+    }       
+}
+
+function highPrice(){
+    results.innerHTML = `<p></p>`;
+    for (high_price in products.price){
+        if (products.price[high_price] > 700 & products.price[high_price] <= 1500){
+            results.innerHTML += `${products.name[high_price]} | ${products.price[high_price]} <br>`
+        }       
+    }       
+}
+        
+
